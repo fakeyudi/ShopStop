3e2f7a4f2144f086835b3a0154cfdea35b90c1fd
YUDI <udixit419@gmail.com>
2020-03-28 21:45:40 +0530

    UI Added

--- ShopStop/Essentials Controllers/EssentialsTableViewCell.swift
+++ ShopStop/Essentials Controllers/EssentialsTableViewCell.swift
@@ -8,19 +8,19 @@
 
 import UIKit
 
-class EssentialsTableViewCell: UITableViewCell {
-
+class EssentialsTableViewCell: UITableViewCell{
+    
     @IBOutlet weak var myText: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+     override func awakeFromNib() {
+         super.awakeFromNib()
+         // Initialization code
+     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+     override func setSelected(_ selected: Bool, animated: Bool) {
+         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
+         // Configure the view for the selected state
+     }
     
 }
